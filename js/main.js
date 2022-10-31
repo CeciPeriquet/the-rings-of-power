1bdbf92700ef2764b17c003968c7d87b0c3084d9
Cecilia Periquet <periwhat@gmail.com>
2022-10-31 11:07:40 +0100

    Try b onus

--- js/main.js
+++ js/main.js
@@ -3,38 +3,70 @@
 const chosenFighter = document.querySelector('.battle-fighter');
 const startButton = document.querySelector('.battle-start');
 const battleResult = document.querySelector('.battle-result');
+let result = "";
+const humanFighter = document.querySelector('.human-fighter');
+const computerFighter = document.querySelector('.computer-fighter');
+let resultValue = "";
+
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
   }
 
-  function getValues (){
+
+function getPoints(){
+let sumResult= "";
+    function sum (){
+        sumResult = parseInt(resultValue) + 1;
+        return sumResult;
+    }
+    let suma = sumResult;
+
+    if (resultValue === 1){
+        computerFighter.innerHTML += suma;
+            console.log('funciona 1')
+
+    } else if (resultValue === 2){
+        console.log('funciona 2')
+        humanFighter.innerHTML += suma;
+    } else if (resultValue === 3){
+        console.log('funciona 3')
+    } 
+}
+
+function getValues (){
     const fighterValue = parseInt(chosenFighter.value);
     console.log(fighterValue);
     const computerValue = getRandomNumber(6);
     console.log(computerValue);
     if (fighterValue < computerValue) {
         const result1 = '¡Ha ganado el ejército del MAL! Vuelve a intentarlo';
-        battleResult.innerHTML = result1;
+        result = result1;
+        resultValue = 1;
        
     } else if (fighterValue > computerValue) {
         const result2 = '¡Ha ganado el ejército del BIEN! ¡Enhorabuena!';
-        battleResult.innerHTML = result2;
+        result = result2;
+        resultValue = 2;
         
     } else if (fighterValue === computerValue) {
         const result3 = '¡EMPATE!';
-        battleResult.innerHTML = result3;
+        result = result3;
+        resultValue = 3;
+        
     }
+    battleResult.innerHTML = result;
+    console.log(result);
+    
+     getPoints(result);
+    
 }
+
+
 
 
 function handleClick (){
     getValues();
-  
 }
-
-/* battleResult.innerHTML = 'holi'; */
-
-
 
 startButton.addEventListener('click', handleClick);
